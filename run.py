c998dac40ba565a8a3797793fb4436471b0ce7a8
kult0922 <38486600+kult0922@users.noreply.github.com>
2020-04-03 22:19:49 +0900

    white color crop

--- run.py
+++ run.py
@@ -1,11 +1,31 @@
 import cv2
 import numpy as np
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("imagePath")
-
+parser.add_argument("--backColor", type=str)
 args = parser.parse_args()
 
+save_dir = os.path.dirname(args.imagePath)
+file_name = os.path.basename(args.imagePath)
+name, file_extension = os.path.splitext(file_name)
+
 image = cv2.imread(args.imagePath)
+
+forward_index = np.where(image != [255, 255, 255])
+
+right = max(forward_index[0])
+left = min(forward_index[0])
+bottom = max(forward_index[1])
+top = min(forward_index[1])
+
+#import pdb;pdb.set_trace()
+croped_image = image[left:right, top:bottom]
+
+save_name = name + "-crop" + file_extension
+save_path = os.path.join(save_dir, save_name)
+
+cv2.imwrite(save_path, croped_image)
 
