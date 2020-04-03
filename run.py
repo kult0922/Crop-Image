9c2d6065881e9c8ca595ff9e661e6d1f00bfea14
kult0922 <38486600+kult0922@users.noreply.github.com>
2020-04-03 22:31:02 +0900

    enable color select

--- run.py
+++ run.py
@@ -3,29 +3,43 @@ import numpy as np
 import argparse
 import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument("imagePath")
-parser.add_argument("--backColor", type=str)
-args = parser.parse_args()
+def cropByColor(image, color):
+    forward_index = np.where(image != color)
 
-save_dir = os.path.dirname(args.imagePath)
-file_name = os.path.basename(args.imagePath)
-name, file_extension = os.path.splitext(file_name)
+    right = max(forward_index[0])
+    left = min(forward_index[0])
+    bottom = max(forward_index[1])
+    top = min(forward_index[1])
 
-image = cv2.imread(args.imagePath)
+    return image[left:right, top:bottom]
 
-forward_index = np.where(image != [255, 255, 255])
+def main():
+    # args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("imagePath")
+    parser.add_argument("--backColor", default="white", type=str)
+    args = parser.parse_args()
 
-right = max(forward_index[0])
-left = min(forward_index[0])
-bottom = max(forward_index[1])
-top = min(forward_index[1])
+    # file information
+    save_dir = os.path.dirname(args.imagePath)
+    file_name = os.path.basename(args.imagePath)
+    name, file_extension = os.path.splitext(file_name)
 
-#import pdb;pdb.set_trace()
-croped_image = image[left:right, top:bottom]
+    # color set
+    if args.backColor == "white":
+        color = [255, 255, 255]
+    if args.backColor == "black":
+        color = [0, 0, 0]
 
-save_name = name + "-crop" + file_extension
-save_path = os.path.join(save_dir, save_name)
+    image = cv2.imread(args.imagePath)
 
-cv2.imwrite(save_path, croped_image)
+    croped_image = cropByColor(image, [255, 255, 255])
+
+    # save setting
+    save_name = name + "-crop" + file_extension
+    save_path = os.path.join(save_dir, save_name)
+    cv2.imwrite(save_path, croped_image)
+
+if __name__ == main:
+    main()
 
