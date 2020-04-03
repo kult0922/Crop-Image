7f16332c314296265c0ad71dd06d41374c5bea15
kult0922 <38486600+kult0922@users.noreply.github.com>
2020-04-03 23:18:16 +0900

    apply alpah channel images

--- run.py
+++ run.py
@@ -3,8 +3,18 @@ import numpy as np
 import argparse
 import os
 
+def cropByAlpha(image):
+    forward_index = np.where(image[:,:,3] != 0)
+
+    right = max(forward_index[0])
+    left = min(forward_index[0])
+    bottom = max(forward_index[1])
+    top = min(forward_index[1])
+
+    return image[left:right, top:bottom]
+
 def cropByColor(image, color):
-    forward_index = np.where(image != color)
+    forward_index = np.where(image[:,:] != color)
 
     right = max(forward_index[0])
     left = min(forward_index[0])
@@ -25,21 +35,28 @@ def main():
     file_name = os.path.basename(args.imagePath)
     name, file_extension = os.path.splitext(file_name)
 
+    image = cv2.imread(args.imagePath, cv2.IMREAD_UNCHANGED)
+
     # color set
     if args.backColor == "white":
         color = [255, 255, 255]
     if args.backColor == "black":
         color = [0, 0, 0]
 
-    image = cv2.imread(args.imagePath)
-
-    croped_image = cropByColor(image, [255, 255, 255])
+    # crop image
+    print(image.shape)
+    if image.shape[2] == 3:
+        croped_image = cropByColor(image, color)
+        print("color")
+    elif image.shape[2] == 4:
+        croped_image = cropByAlpha(image)
+        print("alpha")
 
     # save setting
     save_name = name + "-crop" + file_extension
     save_path = os.path.join(save_dir, save_name)
     cv2.imwrite(save_path, croped_image)
 
-if __name__ == main:
+if __name__ == "__main__":
     main()
 
